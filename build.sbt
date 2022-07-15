bc9823720f167a4d47588dec38cb4631a3f148d6
pravin2015 <pravin.chavan999@gmail.com>
2022-07-15 19:35:37 +0530

    test commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "ScalaProject"
+
+version := "0.1"
+
+scalaVersion := "2.13.8"
+
+idePackagePrefix := Some("com.pc")
---
+++ src/main/scala/ScalaRunner.scala
@@ -0,0 +1,7 @@
+package com.pc
+
+object ScalaRunner {
+  def main(args: Array[String]): Unit = {
+    print("Hello there!!! I love Biryani")
+  }
+}
